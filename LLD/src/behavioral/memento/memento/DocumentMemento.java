4dd30b9944b1490eda2159203c0dce9bfc6a8d5e
Sumit20399 <sumitraj20399@gmail.com>
2024-09-22 14:29:14 +0530

    Implemented memento design pattern

---
+++ LLD/src/behavioral/memento/memento/DocumentMemento.java
@@ -0,0 +1,13 @@
+package behavioral.memento.memento;
+
+public class DocumentMemento {
+    private String content;
+
+    public DocumentMemento(String content) {
+        this.content = content;
+    }
+
+    public String getSavedContent() {
+        return this.content;
+    }
+}
